f30971257b86492cd44381f4538b7f44c10f291e
MadNake <maksim.usalko@mail.ru>
2022-04-07 22:52:05 +0300

    js

--- js/giftset__script.js
+++ js/giftset__script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let giftset__link = document.querySelectorAll(".giftset__slide-bar-item");
+
+for (let i = 0; i < giftset__link.length; i++) {
+
+  giftset__link[i].addEventListener("click", function(evt) {
+      evt.preventDefault();
+      for (var i = 0; i < giftset__link.length; i++) {
+      giftset__link[i].classList.remove("giftset__slide-bar-item_active");
+      }
+
+  })
+}
