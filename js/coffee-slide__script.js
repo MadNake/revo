5fa193973945c1d91ecc01d141474fe1fdf1fff7
MadNake <72208527+MadNake@users.noreply.github.com>
2022-04-13 14:45:09 +0300

    added coffee-slide script (not finished)

---
+++ js/coffee-slide__script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let btn_prev = document.querySelector(".coffee__slide-btn_prev");
+
+let btn_next = document.querySelector(".coffee__slide-btn_next");
+
+let items = document.querySelectorAll(".coffee__grid-item");
+
+btn_next.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  items[0].
+})
