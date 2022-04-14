288d6e5f4b76469d97b0f2e1f280488cf4057abf
MadNake <72208527+MadNake@users.noreply.github.com>
2022-04-14 14:50:07 +0300

    Add files via upload

--- js/coffee-slide__script.js
+++ js/coffee-slide__script.js
@@ -4,9 +4,22 @@ let btn_prev = document.querySelector(".coffee__slide-btn_prev");
 
 let btn_next = document.querySelector(".coffee__slide-btn_next");
 
-let items = document.querySelectorAll(".coffee__grid-item");
+let items = document.querySelectorAll(".coffee__grid-item"),
+beforeZone = document.querySelector(".side-zone_before"),
+itemsLength = items.length;
 
-btn_next.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  items[0].
-})
+//--------------------------------
+
+function test() {
+  for (let i = 0; i < itemsLength.length; i++) {
+  beforeZone.insertBefore(items[0],items[i * 2 + 1])
+};
+};
+test();
+
+//---- Код вверху не работает-----------
+
+//btn_next.addEventListener("click", function(evt) {
+//  evt.preventDefault();
+//  items[0].
+//})
