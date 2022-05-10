5e248218e1b2baae30814f132cdfcd33a12e59b6
MadNake <72208527+MadNake@users.noreply.github.com>
2022-05-10 11:49:51 +0300

    Add files via upload

--- js/coffee-slide__script.js
+++ js/coffee-slide__script.js
@@ -4,32 +4,20 @@ let btn_prev = document.querySelector(".coffee__slide-btn_prev");
 
 let btn_next = document.querySelector(".coffee__slide-btn_next");
 
+let slider = document.querySelector(".coffee__grid-conteiner_active-zone")
+
 let items = document.querySelectorAll(".coffee__grid-item"),
 beforeZone = document.querySelector(".side-zone_before"),
 itemsLength = items.length;
 
+//////////////////////////////
 
+$(function() {
 
+$('.coffee__lider').slick({
+  infininte: true,
+  slidesToShow: 2,
+  slidesToScroll: 2
+});
 
-// let login = prompt("Введите логин","");
-// let password;
-//
-// function passwordChek() {
-//   password = prompt("Введите пароль", "");
-//   if (password == "Я главный") {
-//     alert("Здравствуйте")
-//   } else if (password === "" || password === null) {
-//     alert("Отменено")
-//   } else {
-//      alert("Неверный пароль");
-//      passwordChek();
-//   }
-// }
-//
-// if (login === "admin") {
-//   passwordChek();
-// } else if (login === "" || login === null) {
-//   alert("Отменено")
-// } else {
-//   alert("Я вас не знаю")
-// };
+})
