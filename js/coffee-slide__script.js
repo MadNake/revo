d35c9e174e078ceb8c1b01736cf2de91eae1597d
MadNake <72208527+MadNake@users.noreply.github.com>
2022-06-06 19:49:57 +0300

    Add files via upload

--- js/coffee-slide__script.js
+++ js/coffee-slide__script.js
@@ -1,20 +1,9 @@
 "use strict";
 
-let btn_prev = document.querySelector(".coffee__slide-btn_prev");
-
-let btn_next = document.querySelector(".coffee__slide-btn_next");
-
-let slider = document.querySelector(".coffee__grid-conteiner_active-zone")
-
-let items = document.querySelectorAll(".coffee__grid-item"),
-beforeZone = document.querySelector(".side-zone_before"),
-itemsLength = items.length;
-
-//////////////////////////////
-
-$(function() {
-$('.combo__carts').slick({
-
+$(document).ready(function(){
+  $('.combo__carts').slick({
+    infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+  });
 });
-
-})
